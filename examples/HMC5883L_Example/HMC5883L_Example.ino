358232d15222df1f63519a83483cb5e745ffd5dd
tichise <ichise@gmail.com>
2019-09-15 21:28:41 +0900

    localize

--- examples/HMC5883L_Example/HMC5883L_Example.ino
+++ examples/HMC5883L_Example/HMC5883L_Example.ino
@@ -1,12 +1,12 @@
-/*****************************************************************************/	
-//	Function:	 Get the Geographic direction of the X-axis.
-//				If X-axis points to the North, it is 0 degree.
-//				If X-axis points to the East, it is 90 degrees.
-//				If X-axis points to the South, it is 180 degrees.
-//				If X-axis points to the West, it is 270 degrees.
+/*****************************************************************************/
+// 機能：X軸の地理的方向を取得します。
+// X軸が北を指す場合、0度です。
+// X軸が東を指す場合、90度です。
+// X軸が南を指す場合、180度です。
+// X軸が西を指す場合、270度です。
 //  Hardware:   Grove - 3-Axis Digital Compass
 //	Arduino IDE: Arduino-1.0
-//	Author:	 Frankie.Chu		
+//	Author:	 Frankie.Chu
 //	Date: 	 Jan 10,2013
 //	Version: v1.0
 //
@@ -32,56 +32,52 @@
 //
 /*******************************************************************************/
 
-// Reference the I2C Library
+// I2C Libraryを参照する
 #include <Wire.h>
-// Reference the HMC5883L Compass Library
+// HMC5883Lコンパスライブラリを参照する
 #include <HMC5883L.h>
 
 //#include "Streaming.h"
 
-// Store our compass as a variable.
+// コンパスを変数として保存します。
 HMC5883L compass;
-// Record any errors that may occur in the compass.
+//コンパスで発生する可能性のあるエラーを記録します。
 int error = 0;
 
 MagnetometerScaled valueOffset;
 
-
-// Out setup routine, here we will configure the microcontroller and compass.
+//アウトセットアップルーチン、ここではマイクロコントローラーとコンパスを構成します。
 void setup()
 {
-  // Initialize the serial port.
-  Serial.begin(115200);
+  //シリアルポートを初期化します。  Serial.begin(115200);
   delay(2000);
   Serial.println("Starting the I2C interface.");
   Wire.begin(); // Start the I2C interface.
 
   Serial.println("Constructing new HMC5883L");
-    
+
   Serial.println("Setting scale to +/- 1.3 Ga");
   error = compass.setScale(1.3); // Set the scale of the compass.
-  if(error != 0) // If there is an error, print it out.
+  if (error != 0)                // If there is an error, print it out.
     Serial.println(compass.getErrorText(error));
-  
+
   Serial.println("Setting measurement mode to continous.");
   error = compass.setMeasurementMode(MEASUREMENT_CONTINUOUS); // Set the measurement mode to Continuous
-  if(error != 0) // If there is an error, print it out.
+  if (error != 0)                                             // If there is an error, print it out.
     Serial.println(compass.getErrorText(error));
-    
+
   compassCalibrate();
 }
 
-// calibrate offset of x, y and z
+// x、y、zのオフセットを調整します
 void compassCalibrate(void)
 {
-  //Serial << ">>>> calibrate the compass\n";
   Serial.println("calibrate the compass");
   MagnetometerScaled valueMax = {0, 0, 0};
   MagnetometerScaled valueMin = {0, 0, 0};
 
-  // calculate x, y and z offset
-  
-  //Serial << "please rotate the compass" << endl;
+  // x、y、zオフセットを計算します
+
   Serial.println("please rotate the compass");
   int xcount = 0;
   int ycount = 0;
@@ -90,67 +86,92 @@ void compassCalibrate(void)
   boolean yZero = false;
   boolean zZero = false;
   MagnetometerScaled value;
-  while (xcount < 3 || ycount < 3 || zcount < 3) {
+  while (xcount < 3 || ycount < 3 || zcount < 3)
+  {
     value = compass.readScaledAxis();
-    if ((fabs(value.XAxis) > 600) || (fabs(value.YAxis) > 600) || (fabs(value.ZAxis) > 600)) {
+    if ((fabs(value.XAxis) > 600) || (fabs(value.YAxis) > 600) || (fabs(value.ZAxis) > 600))
+    {
       continue;
     }
-    
-    if (valueMin.XAxis > value.XAxis) {
+
+    if (valueMin.XAxis > value.XAxis)
+    {
       valueMin.XAxis = value.XAxis;
-    } else if (valueMax.XAxis < value.XAxis) {
+    }
+    else if (valueMax.XAxis < value.XAxis)
+    {
       valueMax.XAxis = value.XAxis;
     }
-    
-    if (valueMin.YAxis > value.YAxis) {
+
+    if (valueMin.YAxis > value.YAxis)
+    {
       valueMin.YAxis = value.YAxis;
-    } else if (valueMax.YAxis < value.YAxis) {
+    }
+    else if (valueMax.YAxis < value.YAxis)
+    {
       valueMax.YAxis = value.YAxis;
     }
-    
-    if (valueMin.ZAxis > value.ZAxis) {
+
+    if (valueMin.ZAxis > value.ZAxis)
+    {
       valueMin.ZAxis = value.ZAxis;
-    } else if (valueMax.ZAxis < value.ZAxis) {
+    }
+    else if (valueMax.ZAxis < value.ZAxis)
+    {
       valueMax.ZAxis = value.ZAxis;
     }
-    
-    
-    if (xZero) {
-      if (fabs(value.XAxis) > 50) {
+
+    if (xZero)
+    {
+      if (fabs(value.XAxis) > 50)
+      {
         xZero = false;
         xcount++;
       }
-    } else {
-      if (fabs(value.XAxis) < 40) {
+    }
+    else
+    {
+      if (fabs(value.XAxis) < 40)
+      {
         xZero = true;
       }
     }
-    
-    if (yZero) {
-      if (fabs(value.YAxis) > 50) {
+
+    if (yZero)
+    {
+      if (fabs(value.YAxis) > 50)
+      {
         yZero = false;
         ycount++;
       }
-    } else {
-      if (fabs(value.YAxis) < 40) {
+    }
+    else
+    {
+      if (fabs(value.YAxis) < 40)
+      {
         yZero = true;
       }
     }
-    
-    if (zZero) {
-      if (fabs(value.ZAxis) > 50) {
+
+    if (zZero)
+    {
+      if (fabs(value.ZAxis) > 50)
+      {
         zZero = false;
         zcount++;
       }
-    } else {
-      if (fabs(value.ZAxis) < 40) {
+    }
+    else
+    {
+      if (fabs(value.ZAxis) < 40)
+      {
         zZero = true;
       }
     }
-    
+
     delay(30);
   }
-  
+
   valueOffset.XAxis = (valueMax.XAxis + valueMin.XAxis) / 2;
   valueOffset.YAxis = (valueMax.YAxis + valueMin.YAxis) / 2;
   valueOffset.ZAxis = (valueMax.ZAxis + valueMin.ZAxis) / 2;
@@ -160,7 +181,7 @@ void compassCalibrate(void)
   Serial << "offset: " << valueOffset.XAxis << '\t' << valueOffset.YAxis << '\t' << valueOffset.ZAxis << endl;
   
   Serial << "<<<<" << endl;
-#endif  
+#endif
   Serial.print("max: ");
   Serial.print(valueMax.XAxis);
   Serial.print(valueMax.YAxis);
@@ -175,91 +196,92 @@ void compassCalibrate(void)
   Serial.println(valueOffset.ZAxis);
 }
 
-// Our main program loop.
+// メインプログラムループ。
 void loop()
 {
-  // Retrive the raw values from the compass (not scaled).
+  //コンパスから生の値を取得します（スケーリングされません）。
   MagnetometerRaw raw = compass.readRawAxis();
-  // Retrived the scaled values from the compass (scaled to the configured scale).
+
+  //コンパスからスケーリングされた値を取得しました（構成されたスケールにスケーリングされます）。
   MagnetometerScaled scaled = compass.readScaledAxis();
-  
+
   scaled.XAxis -= valueOffset.XAxis;
   scaled.YAxis -= valueOffset.YAxis;
   scaled.ZAxis -= valueOffset.ZAxis;
-  
-  // Values are accessed like so:
-  int MilliGauss_OnThe_XAxis = scaled.XAxis;// (or YAxis, or ZAxis)
 
-  // Calculate heading when the magnetometer is level, then correct for signs of axis.
+  //値は次のようにアクセスされます：
+  int MilliGauss_OnThe_XAxis = scaled.XAxis; // (or YAxis, or ZAxis)
+
+  //磁力計が水平になったら方位を計算し、軸の兆候を修正します。
   float yxHeading = atan2(scaled.YAxis, scaled.XAxis);
   float zxHeading = atan2(scaled.ZAxis, scaled.XAxis);
-  
+
   float heading = yxHeading;
-  
-  // Once you have your heading, you must then add your 'Declination Angle', which is the 'Error' of the magnetic field in your location.
-  // Find yours here: http://www.magnetic-declination.com/
-  // Mine is: -2��37' which is -2.617 Degrees, or (which we need) -0.0456752665 radians, I will use -0.0457
-  // If you cannot find your Declination, comment out these two lines, your compass will be slightly off.
-  float declinationAngle = -0.0457;
+
+  //見出しを取得したら、「偏角」を追加する必要があります。「偏角」は、現在地の磁場の「誤差」です。
+     //ここで見つけてください：http://www.magnetic-declination.com/
+   //私の場合：-2��37 '-2.617度、または（必要な）-0.0456752665ラジアン、-0.0457を使用します
+   //赤緯が見つからない場合は、これら2行をコメントアウトすると、コンパスが少し外れます。
+      float declinationAngle = -0.0457;
   heading += declinationAngle;
-  
-  // Correct for when signs are reversed.
-  if(heading < 0)
-    heading += 2*PI;
-    
+
+  //符号が逆になっている場合に修正します。
+  if (heading < 0)
+    heading += 2 * PI;
+
+  // 赤緯の追加によるラップを確認します。
   // Check for wrap due to addition of declination.
-  if(heading > 2*PI)
-    heading -= 2*PI;
-   
-  // Convert radians to degrees for readability.
-  float headingDegrees = heading * 180/M_PI; 
-  
+  if (heading > 2 * PI)
+    heading -= 2 * PI;
+
+  //読みやすくするためにラジアンを度に変換します。
+  float headingDegrees = heading * 180 / M_PI;
+
   float yxHeadingDegrees = yxHeading * 180 / M_PI;
   float zxHeadingDegrees = zxHeading * 180 / M_PI;
 
-  // Output the data via the serial port.
+  //シリアルポート経由でデータを出力します。
   // Output(raw, scaled, heading, headingDegrees);
-  
-//  Serial << scaled.XAxis << ' ' << scaled.YAxis << ' ' << scaled.ZAxis << endl;
-//  Serial << "arctan y/x: " << yxHeadingDegrees << " \tarctan z/x: " << zxHeadingDegrees << endl;
-  
+
+  //  Serial << scaled.XAxis << ' ' << scaled.YAxis << ' ' << scaled.ZAxis << endl;
+  //  Serial << "arctan y/x: " << yxHeadingDegrees << " \tarctan z/x: " << zxHeadingDegrees << endl;
+
   Serial.print(scaled.XAxis);
   Serial.print(scaled.YAxis);
   Serial.println(scaled.ZAxis);
-  
+
   Serial.print("arctan y/x: ");
   Serial.print(yxHeadingDegrees);
-  Serial.print("arctan z/x: ");  
+  Serial.print("arctan z/x: ");
   Serial.print(zxHeadingDegrees);
-  
-  // Normally we would delay the application by 66ms to allow the loop
-  // to run at 15Hz (default bandwidth for the HMC5883L).
-  // However since we have a long serial out (104ms at 9600) we will let
-  // it run at its natural speed.
-  delay(1000);//of course it can be delayed longer.
+
+  // 通常、ループを許可するためにアプリケーションを66ms遅延させます
+  // 15Hzで実行します（HMC5883Lのデフォルト帯域幅）。
+     // ただし、長いシリアルアウト（9600で104ミリ秒）があるため、
+                  //自然な速度で実行されます。
+      delay(1000); //　もちろん、より長く遅延させることができます。
 }
 
-// Output the data down the serial port.
+//データをシリアルポートに出力します。
 void Output(MagnetometerRaw raw, MagnetometerScaled scaled, float heading, float headingDegrees)
 {
-   Serial.print("Raw:\t");
-   Serial.print(raw.XAxis);
-   Serial.print("   ");   
-   Serial.print(raw.YAxis);
-   Serial.print("   ");   
-   Serial.print(raw.ZAxis);
-   Serial.print("   \tScaled:\t");
-   
-   Serial.print(scaled.XAxis);
-   Serial.print("   ");   
-   Serial.print(scaled.YAxis);
-   Serial.print("   ");   
-   Serial.print(scaled.ZAxis);
+  Serial.print("Raw:\t");
+  Serial.print(raw.XAxis);
+  Serial.print("   ");
+  Serial.print(raw.YAxis);
+  Serial.print("   ");
+  Serial.print(raw.ZAxis);
+  Serial.print("   \tScaled:\t");
 
-   Serial.print("   \tHeading:\t");
-   Serial.print(heading);
-   Serial.print(" Radians   \t");
-   Serial.print(headingDegrees);
-   Serial.println(" Degrees   \t");
+  Serial.print(scaled.XAxis);
+  Serial.print("   ");
+  Serial.print(scaled.YAxis);
+  Serial.print("   ");
+  Serial.print(scaled.ZAxis);
+
+  Serial.print("   \tHeading:\t");
+  Serial.print(heading);
+  Serial.print(" Radians   \t");
+  Serial.print(headingDegrees);
+  Serial.println(" Degrees   \t");
 }
-
